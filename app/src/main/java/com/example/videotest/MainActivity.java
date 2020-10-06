bffdb69cf2da6a898e0c89ea88e66672b392958c
samirmimouni <samirmimouni33@gmail.com>
2020-10-06 11:20:05 +0200

    test

--- app/src/main/java/com/example/videotest/MainActivity.java
+++ app/src/main/java/com/example/videotest/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends Activity {
                 Intent browserIntent =
                         new Intent(Intent.ACTION_VIEW, Uri.parse("/"));
                 startActivity(browserIntent);
-
+//hh
             }
 
         });
